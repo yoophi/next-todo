4a680cf1e6801232d4edcd46aa9729f26c88386f
Pyunghyuk Yoo <yoophi@gmail.com>
2021-03-16 00:46:39 +0900

    🎨 NEW: apply reset css

--- styles/GlobalStyle.ts
+++ styles/GlobalStyle.ts
@@ -1,8 +1,10 @@
 import { createGlobalStyle } from "styled-components";
+import reset from "styled-reset";
 
 const GlobalStyle = createGlobalStyle`
-  body {
-      margin: 0;
+  ${reset}
+  * {
+      box-sizing: border-box;
   }
 `;
 
